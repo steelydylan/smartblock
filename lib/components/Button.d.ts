49285ed9df0dfb20444eee43f9faa1b0286fb150
steelydylan <ess_president@me.com>
2021-01-12 22:23:33 +0900

    chore: delete

--- lib/components/Button.d.ts
+++
@@ -1,7 +0,0 @@
-import * as React from 'react';
-declare type ButtonProps = {
-    active?: boolean;
-    color?: 'black' | 'white';
-};
-declare const _default: import("styled-components").StyledComponent<({ tag, children, ...props }: any) => React.DetailedReactHTMLElement<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>, any, ButtonProps, never>;
-export default _default;
